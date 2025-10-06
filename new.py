070761a0e8a42dea3d0b62039ba0b0682f8c2910
om7 <oriom7maz@gmail.com>
2025-10-06 14:09:12 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,3 +1,14 @@
 import os
-
+import time
+print("Hello.")
+time.slepp(1)
+print("This is just a test.")
+time.sleep(1)
+print("I will close this code soon")
+time.sleep("1")
+print("Just joking,I hacked Ur ass")
+time.sleep(1)
+print("00")
+os.system("cmatrix")
+time.sleep(4)
 os.system("clear")
